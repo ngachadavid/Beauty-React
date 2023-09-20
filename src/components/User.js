0d3ff56845114f54d1b2ba10c1aea9ffe1affe77
David <davidngacha01@gmail.com>
2023-09-20 18:55:01 +0300

    feat: add user js component

---
+++ src/components/User.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+function User() {
+    return (
+<FontAwesomeIcon icon={faUser} size="lg" />    )
+}
+
+export default User
