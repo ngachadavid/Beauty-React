fced4d57800f9b758ff700c282b94a42e5308c35
David <davidngacha01@gmail.com>
2023-09-22 19:10:43 +0300

    fix: create search function return

--- src/components/Search.js
+++ src/components/Search.js
@@ -13,4 +13,25 @@ const Search = ({ onSearch }) => {
 
   return (
 
-<
+<form className="flex items-center">   
+    <div className="relative w-full">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <FontAwesomeIcon icon={faSearch} />
+        </div>
+        <input 
+          type="text" 
+          className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+          placeholder="Search for items" 
+          value={query}
+          onChange={handleSearchChange}
+
+          />
+    </div>
+
+</form>
+
+ 
+  );
+};
+
+export default Search;
