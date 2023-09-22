bfc758fad287b12b1756959ce336193aeebb8604
David <davidngacha01@gmail.com>
2023-09-22 14:29:37 +0300

    fix: production list returns

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -27,3 +27,47 @@ const ProductList = ({ category, products, setProducts }) => {
   }
 
   
+return (
+
+<div >
+    {/* <Sidebar setCategory={setCategories} /> */}
+    <div className="grid grid-cols-4 gap-4 p-4">
+        {filteredProducts.map((product) => (
+          <div key={product.id} className="card bg-white rounded-lg  p-2 relative w-full max-w-sm border border-gray-200  shadow dark:bg-gray-800 dark:border-gray-700 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110">
+             <div className='rounded-lg '>
+               <img 
+              className="object-cover p-2 rounded-t-lg"
+               src={product.image_url} 
+               alt="beauty products"
+                />
+            </div>
+            <div className="px-5 pb-5">
+              <Link
+                to={`/product/${product.id}`}
+                className="text-sm font-semibold cursor-pointer  hover:text-none text-grey"
+              >
+                {product.name}
+              </Link>
+              <div className="flex items-center justify-between">
+
+              <Link 
+              to={`/product/${product.id}`}
+              className=" pricing text-l font-bold text-gray-900 dark:text-white">
+                ksh {product.price}
+              </Link>
+             </div>
+
+            </div>
+           
+          </div>
+        ))}
+    </div>
+    </div>
+    );
+  };
+  
+
+
+export default ProductList;
+
+
