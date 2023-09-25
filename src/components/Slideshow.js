d2fd3039dd7e62cdfb23224be32e74c08e74913a
David <davidngacha01@gmail.com>
2023-09-25 16:40:59 +0300

    fix: create slideshow functionality

---
+++ src/components/Slideshow.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'flowbite/dist/flowbite.min.css';
+import 'flowbite/dist/flowbite.min.js';
+import toner from "../images/toner.jpg"
+import sunscreen from "../images/sunsceen.jpg"
+import eyeshadow from "../images/eyeshadow.jpg"
+import cream from "../images/cream.jpg"
+import shampoo from "../images/shampoo.jpg"
+import roller from "../images/roller.jpg"
+
+import { Link } from 'react-router-dom';
+
+function Slideshow() {    
+
+   
