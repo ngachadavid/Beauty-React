6fdef16891c2328879b9babb166c9e4b4117d150
David <davidngacha01@gmail.com>
2023-10-02 15:44:20 +0300

    fix: add imports on the app js component

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import '../App.css'
+import LandingPage from '../pages/LandingPage'
+import Navbar from './Navbar';
+import ProductPage from "../pages/ProductPage"
+import { Routes, Route, useNavigate } from "react-router-dom";
+import ProductsInfoPage from './ProductsInfo';
+import ShoppingCart  from '../pages/ShoppingCart'
+import Slideshow from '../components/Slideshow';
+import BillingInfo from '../pages/BillingInfo';
+import Receipt from '../pages/Receipt';
+import Orders from '../pages/Orders';
+import About from '../pages/About';
+import Payment from './Payment';
+import Footer from './Footer';
+import Login from './Login';
+import Signup from './Signup'
+import 'flowbite/dist/flowbite.min.css';
+import 'flowbite/dist/flowbite.min.js';
+import axios from 'axios';
+import React, { useState } from 'react';
+// for the admin panel
+import AdminTable from '../admin/AdminTable';
+import AddProduct from '../admin/AddProduct';
+import Success from '../pages/Success.js';
+
