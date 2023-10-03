5f0107f212d7bdbc27666c9cd17a07e163ef4421
David <davidngacha01@gmail.com>
2023-10-03 19:13:12 +0300

    fix: changed routes convention in app js

--- src/components/App.js
+++ src/components/App.js
@@ -25,66 +25,68 @@ import Success from '../pages/Success.js';
 
 function App() {
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
-    const navigate = useNavigate()
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      // navigate("/admin-table");
-  
-      try {
-        const response = await axios.post('https://beaty-product-shop.onrender.com/login', {
-          username,
-          password,
-        });
-        console.log(response.data);
-        const { name, email } = response.data;
-        console.log(`Welcome, ${name}! Your email is ${email}.`);
-        setIsLoggedIn(true)
-        navigate("/admin-table");
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const handleLogout = async () => {
-        try {
-            await axios.delete('https://beaty-product-shop.onrender.com/logout');
-            setIsLoggedIn(false);
-            navigate('/login');    
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  const navigate = useNavigate()
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // navigate("/admin-table");
 
-    return (
-        <div className='App'>
-            <Navbar isLoggedIn={isLoggedIn} username={username} handleLogout={handleLogout} />
-            <Routes>
-                <Route path='/' element={<LandingPage/>}/>
-                <Route path='/about' element={<About/>}/>
-                <Route path='/products' element={<ProductPage/>}/>
-                <Route path='/product/:id' element={<ProductsInfoPage/>}/>
-                <Route path='/order' element={<Orders/>}/>
-                <Route path='/cart' element={<ShoppingCart/>}/>
-                <Route path='/billinginfo' element={<BillingInfo/>}/>
-                <Route path='/receipt' element={<Receipt/>}/>
-                <Route path='/payment' element={<Payment/>}/>
-                <Route path="/login" element={<Login handleSubmit={handleSubmit} username={username} password={password} setPassword={setPassword} setUsername={setUsername}/>} /> 
-                <Route path='/signup' element={<Signup/>}/>
-                <Route path='/add-product' element={<AddProduct/>}/>
-                <Route path='/admin-table' element={<AdminTable/>}/>
-                <Route path='/success' element={<Success/>}/>
-                
-            </Routes>
-            {/* <Slideshow/> */}
-            <Footer/>
-        </div>
-    );
+    try {
+      const response = await axios.post('https://beaty-product-shop.onrender.com/login', {
+        username,
+        password,
+      });
+      console.log(response.data);
+      const { name, email } = response.data;
+      console.log(`Welcome, ${name}! Your email is ${email}.`);
+      setIsLoggedIn(true)
+      navigate("/admin-table");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  const handleLogout = async () => {
+    try {
+      await axios.delete('https://beaty-product-shop.onrender.com/logout');
+      setIsLoggedIn(false);
+      navigate('/login');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+
+  return (
+    <div className='App'>
+    <Navbar isLoggedIn={isLoggedIn} username={username} handleLogout={handleLogout} />
+    <Routes>
+      <Route path='/' element={<LandingPage/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path="/products" element={<ProductPage />} />
+      <Route path='/product/:id' element={< ProductsInfoPage/>}/>
+      <Route path="/order" element={<Orders/>} />
+      <Route path="/cart" element={<ShoppingCart/>} />
+      <Route path="/billinginfo" element={<BillingInfo/>} />
+      <Route path="/receipt" element={<Receipt/>} /> 
+      <Route path="/payment" element={<Payment/>} /> 
+      <Route path="/login" element={<Login handleSubmit={handleSubmit} username={username} password={password} setPassword={setPassword} setUsername={setUsername}/>} /> 
+      <Route path="/signup" element={<Signup />} /> 
+      <Route path="/add-product" element={<AddProduct/>} /> 
+      <Route path="/admin-table" element={< AdminTable/>} /> 
+      <Route path="/success" element={< Success/>} /> 
+
+    </Routes>
+    {/* <Slideshow/> */}
+    <Footer/>
+    </div>
+     
+
+  );
 }
 
-export default App;
+export default App; 
