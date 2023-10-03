f41f4ee1fd4cad56b6a1caf37dff47a48843f706
David <davidngacha01@gmail.com>
2023-10-03 17:21:13 +0300

    fix: add handlesubmit and seterror functions

--- src/admin/AddProduct.js
+++ src/admin/AddProduct.js
@@ -26,5 +26,30 @@ const AddProduct = () => {
      setCategory(e.target.value);
    };
  
-   const
+   const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("https://beaty-product-shop.onrender.com/beauty_products", {
+        ...product,
+        category, 
+      });
+      console.log(response.data);
+      setSuccessMsg("Product added successfully!");
+      setProduct({
+        name: "",
+        description: "",
+        price: "",
+        qty: "",
+        image_url: "",
+        category: "",
+      });
+      setError(null); 
+    } catch (error) {
+      console.error(error);
+      setError(
+        "There was an error adding the product. Please try again later."
+      );
+      setSuccessMsg(null);
+    }
+  };
 }
