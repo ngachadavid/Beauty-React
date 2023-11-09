85ad7ea1b87d6d135fc084fe5205ad8f5d942a0d
David <davidngacha01@gmail.com>
2023-11-09 18:06:45 +0300

    fix: add receipt function

--- src/pages/Receipt.js
+++ src/pages/Receipt.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+function Receipt({ cartItems,  formData, handleConfirm  }) {
+    console.log(cartItems)
+
+    console.log(formData)
+
+  const totalPrice = cartItems.reduce((acc, curr) => acc + curr.price * curr.quantity, 0);
+
+
 
