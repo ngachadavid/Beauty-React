d35f187760511bfb9c16097ac3806b922a737697
David <davidngacha01@gmail.com>
2023-11-13 18:40:42 +0300

    fix: landing page imports

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import landingImage from"../images/landingpage.jpg"
+import WhyUs from '../landingcontent/WhyUs';
+import Testimonials from '../landingcontent/Testimonials';
+import SocialProof from '../landingcontent/SocialProof';
+import Slideshow from '../components/Slideshow';
+
+
+    
+ 
+
+export default LandingPage
