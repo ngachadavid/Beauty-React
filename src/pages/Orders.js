e70b1d43a866803ab24dcfc0267a32ca630b592d
David <davidngacha01@gmail.com>
2023-11-14 19:09:06 +0300

    fix: add orders import and functions

---
+++ src/pages/Orders.js
@@ -0,0 +1,100 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import ShoppingCart from './ShoppingCart';
+import BillingInfo from './BillingInfo';
+import Receipt from './Receipt';
+
+const Orders = () => {
+
+  const [cartItems, setCartItems] = useState([]);
+  const [stage, setStage] = useState("cart"); 
+  const [formData, setFormData] = useState({
+    name: '',
+    address: '',
+    phoneNumber: ''
+  });
+
+  const navigate = useNavigate();
+
+
+    
+useEffect(() => {
+    if (cartItems.length === 0) {
+      axios.get('https://api.jsonbin.io/v3/b/645017f19d312622a3559b2a')
+        .then(response => {
+          console.log(response.data.record.cart)
+          setCartItems(response.data.record.cart)
+        })
+        .catch(error => console.log(error))
+    }
+  }, []);
+  
+  console.log(cartItems)
+
+  // remove item from cart
+  const handleDeleteItem = (item) => {
+    axios.delete(`http://localhost:4000/cart/${item.id}`)
+      .then(response => {
+        setCartItems(prevState => prevState.filter(cartItem => cartItem.id !== item.id));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  // edit quantity in cart
+  const handleQuantityChange = (value, item) => {
+    axios.patch(`http://localhost:4000/${item.id}`, { quantity: value })
+      .then(response => {
+        setCartItems(prevState => {
+          const index = prevState.findIndex(cartItem => cartItem.id === item.id);
+          prevState[index].quantity = parseInt(value);
+          return [...prevState];
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+ 
+
+  
+  const handleCheckout = () => {
+    // Clear the cart and set the stage to "billing"
+    // setCartItems([]);
+    setStage("billing");
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // Submit the form data to the server and navigate to the receipt
+    const response = await axios.post('https://beaty-product-shop.onrender.com/customers', formData);
+    const { name, address } = response.data;
+    // setFormData({ name: '', address: '', phoneNumber: '' });
+    setStage("receipt");
+  };
+
+
+
+  const handleConfirm = () => {
+    setCartItems(prevState => {
+      return [];
+    });
+    navigate("/success");
+    console.log(cartItems)
+  };
+  
+
+console.log(formData, cartItems)
+
+ 
+}
+
+export default Orders;
