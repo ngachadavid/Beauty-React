53c4c4e797fc383158159528fce416f1230527b7
David <davidngacha01@gmail.com>
2023-11-15 17:58:28 +0300

    fix: shopping cart imports

---
+++ src/pages/ShoppingCart.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { Link, useNavigate } from "react-router-dom";
+import Receipt from "./Receipt"
+
